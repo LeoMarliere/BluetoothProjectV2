246b937b4e582bdf4d45cb49a2938e73d2f81b45
Marliere Léo <leomarliere@hotmail.fr>
2017-11-02 17:08:04 +0100

    commit

--- src/ui/DeviceList.java
+++ src/ui/DeviceList.java
@@ -1,12 +1,17 @@
 package ui;
 
+import object.Fenetre;
+
 public class DeviceList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Fenetre fen = new Fenetre();
 
 		
-		//test
+	
+		
 	}
 
 }
