5bf0b7a079d9a20bfc12c3be70567e2b804f4e42
Nopac <captheo@hotmail.fr>
2017-11-03 15:21:02 +0100

    Connection BDD + Add .jar

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Connection con;
+		ResultSet rs;
+		String url="jdbc:mysql://localhost/BluetoothProject";
+		String login="root";
+		String password="root";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println(1);
+			con=DriverManager.getConnection(url,login,password);
+			PreparedStatement pstmt =con.prepareStatement("SELECT * FROM peripherique;");
+			//   pstmt.setString(1, "test");
+			rs=pstmt.executeQuery();
+			while(rs.next()){ 
+				System.out.println("test="+rs.getString(2));
+			}
+			
+			pstmt.close();
+			con.close();
+			rs.close();
+		} catch (Exception e) {
+			System.err.println("Unable to find and load driver");
+			System.exit(1);	      
+		}
+
+
+
+	}
+}
+
