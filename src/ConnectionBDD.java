2d2276ecda39bb83a403d4fb850f22a4637b85f2
Nopac <captheo@hotmail.fr>
2017-11-29 10:29:49 +0100

    Connection BDD distante

--- src/ConnectionBDD.java
+++ src/ConnectionBDD.java
@@ -4,31 +4,30 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class ConnectionBDD {
-//	public static void main(String[] args) {
-//		Connection con;
-//		ResultSet rs;
-//		String url="jdbc:mysql://localhost/BluetoothProject";
-//		String login="root";
-//		String password="root";
-//
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//			System.out.println(1);
-//			con=DriverManager.getConnection(url,login,password);
-//			PreparedStatement pstmt =con.prepareStatement("SELECT * FROM peripherique;");
-//			//   pstmt.setString(1, "test");
-//			rs=pstmt.executeQuery();
-//			while(rs.next()){ 
-//				System.out.println("test="+rs.getString(2));
-//			}
-//
-//			pstmt.close();
-//			con.close();
-//			rs.close();
-//		} catch (Exception e) {
-//			System.err.println("Unable to find and load driver");
-//			System.exit(1);	      
-//		}
-//
-//	}
+	public static void main(String[] args) {
+		Connection con;
+		ResultSet rs;
+		String url="jdbc:mysql://mysql-capontheo.alwaysdata.net:3306/capontheo_bluetoothproject";
+		String login="capontheo";
+		String password="root";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection(url,login,password);
+			PreparedStatement pstmt =con.prepareStatement("SELECT * FROM peripherique;");
+			//   pstmt.setString(1, "test");
+			rs=pstmt.executeQuery();
+			while(rs.next()){ 
+				System.out.println("test="+rs.getString(2));
+			}
+			pstmt.close();
+			con.close();
+			rs.close();
+		} catch (Exception e) {
+			System.err.println("Unable to find and load driver");
+			e.printStackTrace();
+			System.exit(1);	      
+		}
+
+	}
 }
