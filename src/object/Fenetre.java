ca79037ec52a23d29cff5d2023eb3ee594255b28
Nopac <captheo@hotmail.fr>
2017-12-05 22:41:29 +0100

    requete + affichage

--- src/object/Fenetre.java
+++ src/object/Fenetre.java
@@ -1,70 +1,79 @@
 package object;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
-import java.util.List;
 
-import javax.bluetooth.RemoteDevice;
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-import ui.RemoteDeviceDiscoverer;
+import ui.DetectedDevicePanel;
 
 public class Fenetre extends JFrame {
-	 private JPanel pan = new JPanel();
-	 private JButton bouton = new JButton("Ajouter");
+	private JTable tableau;
+	private JScrollPane scrollPane;
+	 public Fenetre() throws IOException, InterruptedException{
 
-//	static String tempDeviceName;
-//	private static RemoteDeviceDiscoverer remoteDeviceDiscoverer;
-//	private static List <RemoteDevice> deviceList;
-	
-	
-	 public Fenetre() throws IOException, InterruptedException{ 
-		 
-		 
+		 	// On crée la fenêtre
 		    this.setTitle("Liste des Appareils Bluetooth");
-		    this.setSize(600,800);
+		    this.setSize(400, 500);
 		    this.setLocationRelativeTo(null); 
 		    this.setResizable(false);
 		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		    
+		    tableau = new JTable(new DetectedDevicePanel());
+		    scrollPane =new JScrollPane(tableau);
 		    
-//		    remoteDeviceDiscoverer = new RemoteDeviceDiscoverer();  	
-//			remoteDeviceDiscoverer.searchDevices(); 
-//			deviceList = remoteDeviceDiscoverer.getTest();
-//			System.out.println(remoteDeviceDiscoverer.getTest());
-		 
-		    //Instanciation d'un objet JPanel
-		   
+		    getContentPane().add(scrollPane, BorderLayout.CENTER);
 		    
-		    //Définition de sa couleur de fond
-		    pan.setBackground(Color.ORANGE);  
+		    JPanel boutons = new JPanel();
 		    
-//		    for(int i=0;i<deviceList.size();i++) {
-//		    	JLabel label = new JLabel("\n " + deviceList.get(i).getFriendlyName(false) + "    " + deviceList.get(i).getBluetoothAddress() + "\n");
-//		    	pan.add(label);
-//		    	
-//		    	
-//		    }
+	        boutons.add(new JButton(new RefreshAction()));
+	        boutons.add(new JButton(new ShowFCAction()));
+	 
+	        getContentPane().add(boutons, BorderLayout.SOUTH);
 		    
-		      
-		       
-		    
-		        pan.add(bouton);
-		 
-		        
-		        
-		        
-		    
-		    
-		    //On prévient notre JFrame que notre JPanel sera son content pane
-		    this.setContentPane(pan);               
-		    this.setVisible(true);
-		    
-		  }       
+	        pack();
+
+		  }    
+	 
+	  public static void main(String[] args) throws IOException, InterruptedException {
+	        new Fenetre().setVisible(true);
+	    }
+	  
+	  
+	  private class RefreshAction extends AbstractAction {
+	        private RefreshAction() {
+	            super("Actualiser");
+	        }
+	 
+	        public void actionPerformed(ActionEvent e) {
+	        	try {
+	        		tableau.setModel(new DetectedDevicePanel());
+	    		    scrollPane.setViewportView(tableau);
+				} catch (IOException | InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+	        	 System.out.println("okok");
+	        }
+	    }
+	  
+	  private class ShowFCAction extends AbstractAction {
+	        private ShowFCAction() {
+	            super("Ouvrir Fiche Client");
+	        }
+	 
+	        public void actionPerformed(ActionEvent e) {
+	        	int selection = tableau.getSelectedRow();
+	        	String addresseMac = (String) tableau.getValueAt(selection, 0);
+	        	
+	        }
+	    }
 
 }
